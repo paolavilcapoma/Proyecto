36e8ee8bf0e59b362ae0ee73b841050a07fd3281
MariaVilcapoma <i2011451@continental.edu.pe>
2022-02-11 01:33:18 -0500

    nuevo cambio

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using a__Ejemplo_1.Data;
+using Tailwind;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+if(app.Environment.IsDevelopment()){
+    app.RunTailwind("tailwind", "./");
+}
+app.Run();
